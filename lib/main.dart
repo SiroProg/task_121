bad378af93fe45147f5eb7c2f3bed2c3aae890b3
SiroProg <070777sss@gmail.com>
2023-08-18 07:53:58 +0500

    home work

--- lib/main.dart
+++ lib/main.dart
@@ -1,193 +1,343 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.only(top: 20, left: 10),
-          child: Column(
-            children: [
-              Row(
+        body: Column(
+          children: [
+            Container(
+              height: 265,
+              width: double.infinity,
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Image(
+                image: NetworkImage(
+                  "https://images.uzum.uz/chasgo8bv8as0acr8jug/original.jpg",
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              child: const Text(
+                "Simsiz sensrli naushnik TWS i14, i15, i16, i18, i12, i11 mikrafon va keysi bilan",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                for (int i = 0; i < 5; i++)
+                  Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    child: const Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      top: 20,
+                      bottom: 20,
+                    ),
+                    child: const Text("  4.6(685 sharh 20 136 ta buyurtma)"),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                "   O'lchamni tanlang:",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              alignment: Alignment.topLeft,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      bottom: 20,
+                    ),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      "Shaffof",
+                      style: TextStyle(fontSize: 16),
+                    )),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      bottom: 20,
+                    ),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      "Shaffof",
+                      style: TextStyle(fontSize: 16),
+                    )),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                      bottom: 20,
+                    ),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      "Shaffof",
+                      style: TextStyle(fontSize: 16),
+                    )),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: const Text(
+                      "   Narxi:",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    alignment: Alignment.topLeft,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 20),
+                    child: Text(
+                      "Mavjud: 1681   ",
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 69, 172, 109),
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    alignment: Alignment.topRight,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    "   36 000 so'm / ед.",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Text("       60 000 so'm"),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 120,
+                  height: 20,
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    bottom: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    color: Color.fromARGB(255, 66, 227, 130),
+                  ),
+                  child: Center(child: Text("Maktab boozori")),
+                ),
+              ],
+            ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+                color: const Color.fromARGB(255, 234, 232, 226),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
                 children: [
-                  draw(
-                    const Color.fromARGB(255, 100, 20, 140),
-                    'Circadian\nRhythm TRF',
-                    '13',
-                    true,
-                  ),
-                  draw(
-                    Color.fromARGB(255, 232, 39, 136),
-                    '16:8\nTRF',
-                    '16',
-                    true,
-                  ),
-                  draw(
-                    Color.fromARGB(255, 14, 85, 30),
-                    '18:6\nTRF',
-                    '18',
-                    true,
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "  oyiga 4 800 so'mdan",
+                            style: TextStyle(
+                              backgroundColor: Colors.yellow,
+                            ),
+                          ),
+                          TextSpan(text: "muddatli to'lov"),
+                          TextSpan(
+                            text: "  >",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
-              Row(
-                children: [
-                  draw(
-                    Color.fromARGB(255, 232, 131, 30),
-                    '20:4\nTRF',
-                    '20',
-                    true,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 10,
+                    top: 20,
+                    right: 20,
                   ),
-                  draw(
-                    Color.fromARGB(255, 12, 64, 154),
-                    '36-Hour\nFast',
-                    '36',
-                    true,
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    color: Color.fromARGB(255, 233, 208, 132),
                   ),
-                  draw(
-                    Color.fromARGB(255, 64, 65, 69),
-                    'Custom\nFast',
-                    '1-168',
-                    false,
+                  child: Center(
+                    child: Icon(
+                      Icons.assignment_turned_in_outlined,
+                      size: 25,
+                    ),
                   ),
-                ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "Bu haftada 1483 kishi sotib oldi",
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 70,
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                ),
+                color: Color.fromARGB(255, 228, 226, 222),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 10,
+                      top: 10,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Padding(
-                          padding:
-                              EdgeInsets.only(left: 10, right: 10, top: 30),
-                          child: Text(
-                            'Your Presets',
-                            style: TextStyle(fontWeight: FontWeight.bold),
+                        Container(
+                          child: const Text("Narx"),
+                        ),
+                        Container(
+                          child: const Text(
+                            "36 000 so'm",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                        Padding(
-                          padding:
-                              EdgeInsets.only(left: 10, right: 10, top: 30),
-                          child: Container(
-                            width: 60,
-                            height: 20,
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 239, 59, 27),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Zero',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          ),
-                        )
                       ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Container(
-                        width: 120,
-                        height: 150,
-                        decoration: BoxDecoration(
-                            color: const Color.fromARGB(255, 175, 161, 160),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: Center(
-                          child: Icon(
-                            Icons.add,
-                            size: 30,
-                          ),
+                  ),
+                  Container(
+                    width: 170,
+                    height: 40,
+                    margin: EdgeInsets.only(left: 100),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        color: Color.fromARGB(255, 100, 13, 187)),
+                    child: Center(
+                      child: Text(
+                        "Savatga",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-Widget draw(Color color, String text1, String text2, bool item) {
-  return Expanded(
-    child: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(10),
-        ),
-        color: color,
-      ),
-      width: double.infinity,
-      height: 150,
-      padding: EdgeInsets.all(10),
-      margin: EdgeInsets.all(10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(bottom: 5, top: 10, left: 10),
-            child: Expanded(
-              child: Text(
-                text1,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Text(''),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 10),
-            child: Expanded(
-              child: Text(
-                text2,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 10, right: 20),
-                child: Expanded(
-                  child: Text(
-                    'hours',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 13,
                     ),
                   ),
-                ),
+                ],
               ),
-              if (item)
-                Icon(
-                  Icons.info,
-                  color: Colors.white,
-                )
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     ),
-    flex: 1,
   );
 }
